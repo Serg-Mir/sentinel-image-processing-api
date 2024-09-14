31614e735cd933d23a34f9aa7a0257f5068f3b84
Sergii Miroshnykov <it.sergm@gmail.com>
2024-09-14 10:17:07 +0200

    Modified pre-commit black line length param

--- app/core/logging.py
+++ app/core/logging.py
@@ -6,7 +6,9 @@ def setup_logging():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
 
     file_handler = RotatingFileHandler("app.log", maxBytes=10000000, backupCount=5)
     file_handler.setFormatter(formatter)
