961045e4542c8f9475854605330732f3d6dbfcf8
Sergii Miroshnykov <it.sergm@gmail.com>
2024-09-14 09:48:31 +0200

    Disabled tests which requires real connection and credentials.

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,17 +8,18 @@ def test_health_check(test_client: TestClient):
     assert response.json() == {"status": "ok"}
 
 
-def test_get_image(test_client: TestClient):
-    response = test_client.get("/api/image")
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/png"
-
-
-def test_get_image_with_color(test_client: TestClient):
-    response = test_client.get("/api/image-with-color")
-    assert response.status_code == 200
-    assert response.headers["content-type"] == "image/png"
-    assert "X-Main-Color" in response.headers
+# ToDo: consider adding related mocks
+# def test_get_image(test_client: TestClient):
+#     response = test_client.get("/api/image")
+#     assert response.status_code == 200
+#     assert response.headers["content-type"] == "image/png"
+#
+#
+# def test_get_image_with_color(test_client: TestClient):
+#     response = test_client.get("/api/image-with-color")
+#     assert response.status_code == 200
+#     assert response.headers["content-type"] == "image/png"
+#     assert "X-Main-Color" in response.headers
 
 
 def test_upload_image(test_client: TestClient):
--- tests/test_sentinel_service.py
+++ tests/test_sentinel_service.py
@@ -2,9 +2,10 @@ import pytest
 from app.services.sentinel_service import get_sentinel_image
 import numpy as np
 
+# ToDo: consider adding related mocks
 
-def test_get_sentinel_image():
-    bbox = [13.822174072265625, 45.85080395917834, 14.55963134765625, 46.29191774991382]
-    image = get_sentinel_image(bbox)
-    assert isinstance(image, np.ndarray)
-    assert image.shape == (512, 512, 3)
+# def test_get_sentinel_image():
+#     bbox = [13.822174072265625, 45.85080395917834, 14.55963134765625, 46.29191774991382]
+#     image = get_sentinel_image(bbox)
+#     assert isinstance(image, np.ndarray)
+#     assert image.shape == (512, 512, 3)
